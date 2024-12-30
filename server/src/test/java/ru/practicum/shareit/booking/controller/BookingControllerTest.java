13f83fed8389265d3ec76dfe7390f121ed19f77b
quhead <quququhead@gmail.com>
2024-12-30 14:17:40 +0500

    fix checkstyle

--- server/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BookingControllerTest {
 
     @Mock
-    private BookingService bookingService;;
+    private BookingService bookingService;
 
     @InjectMocks
     private BookingController bookingController;
--- server/src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -20,7 +20,6 @@ import ru.practicum.shareit.user.dal.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
